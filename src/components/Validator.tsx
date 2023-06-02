5d6d9dd42237c680a4601778b7f331fafa7ef30b
hipsterhead91 <virtualxself@gmail.com>
2023-06-02 18:24:50 +0200

    some changes in table row code

--- src/components/Validator.tsx
+++ src/components/Validator.tsx
@@ -23,21 +23,32 @@ function Validator() {
   const navigate = useNavigate();
   const setIsValidatorsHidden = useOutletContext<React.Dispatch<React.SetStateAction<boolean>>>();
 
-
-
+  // ПРИ ОТКРЫТИИ КОМПОНЕНТА СКРЫВАЕМ ТАБЛИЦУ ВАЛИДАТОРОВ
   useEffect(() => {
-    console.log(currentValidator);
-  }, [currentValidator])
+    setIsValidatorsHidden(true);
+  }, [])
+
+  // ВОЗВРАТ К ТАБЛИЦЕ ВАЛИДАТОРОВ
+  const returnToValidators = () => {
+    navigate(`/${currentChain?.chainId}/validators`);
+    setIsValidatorsHidden(false);
+  }
+
+  // ПОЛУЧАЕМ ОБЪЕКТ ТЕКУЩЕГО ВАЛИДАТОРА
+  useEffect(() => {
+    const validator = validators?.find((val: { operator_address: string }) => val.operator_address === currentValoper);
+    if (validator) setCurrentValidator(validator);
+    else setCurrentValidator(null);
+  }, [currentValoper, currentChain, validators])
 
   let avatarUrl, monikerText, valoperText, activityText, activityStyle, bondText, bondStyle, jailText, jailStyle, highCommissionText, highCommissionStyle, rankText, tokensText, symbolText, votingPowerText, commissionText, commissionStyle, websiteText, securityContactText, detailsText;
-  
+
+  // РЕНДЕР ИНФОРМАЦИИ О ВАЛИДАТОРЕ
   if (currentValidator) {
 
-    if (currentValidator.avatar) {
-      avatarUrl = currentValidator.avatar
-    } else {
-      avatarUrl = `${process.env["PUBLIC_URL"]}/images/no-avatar.png`
-    };
+    avatarUrl = (currentValidator.avatar)
+      ? currentValidator.avatar
+      : `${process.env["PUBLIC_URL"]}/images/no-avatar.png`;
 
     monikerText = currentValidator.description.moniker;
     valoperText = currentValidator.operator_address;
@@ -72,16 +83,16 @@ function Validator() {
     if (Number(currentValidator.commission.commission_rates.rate) > 0.1) {
       highCommissionText = "High %";
       highCommissionStyle = "validator__status validator__status_bad";
+      commissionStyle = "validator__commission-value validator__commission-value_high";
     } else {
       highCommissionText = "";
       highCommissionStyle = "validator__status validator__status_hidden";
+      commissionStyle = "validator__commission-value";
     }
 
-    if (currentValidator.rank) {
-      rankText = "#" + currentValidator.rank.toString().padStart(3, '0');
-    } else {
-      rankText = "#000";
-    }
+    rankText = (currentValidator.rank)
+      ? "#" + currentValidator.rank.toString().padStart(3, '0')
+      : "#000";
 
     if (currentChain) {
       tokensText = tweakTokens(currentValidator.tokens, currentChain);
@@ -91,42 +102,31 @@ function Validator() {
       symbolText = "";
     }
 
-    if (currentValidator.voting_power && currentChain) {
-      votingPowerText = tweakVotingPower(currentValidator.voting_power, currentChain) + '%'
-    } else {
-      votingPowerText = "";
-    }
+    votingPowerText = (currentValidator.voting_power && currentChain)
+      ? tweakVotingPower(currentValidator.voting_power, currentChain) + '%'
+      : "";
 
     commissionText = tweakCommission(currentValidator.commission.commission_rates.rate) + '%';
-    if (Number(currentValidator.commission.commission_rates.rate) > 0.1) {
-      commissionStyle = "validator__commission-value validator__commission-value_high";
-    } else {
-      commissionStyle = "validator__commission-value";
-    }
 
     /* В ранней версии вместо websiteText у меня был websiteEl, который в случае отсутствия ссылки превращался в текстовый прочерк, а в случае наличия - в кликабельную ссылку. Однако я обнаружил, что если валидатор указал ссылку не в формате http://site.com, а просто как site.com, то её нельзя передавать в href - ссылка не будет корректно работать. Таких валидаторов меньшинство, но всё же. По этой причине решил пока оставить сайт обычным текстом, но позже возможно напишу для ссылок валидацию, чтобы все варианты работали корректно.  */
-    if (currentValidator.description.website) {
-      websiteText = currentValidator.description.website;
-    } else { 
-      websiteText = "—";
-    }
+    websiteText = (currentValidator.description.website)
+      ? currentValidator.description.website
+      : "—";
 
-    if (currentValidator.description.security_contact) {
-      securityContactText = currentValidator.description.website;
-    } else { 
-      securityContactText = "—";
-    }
+    securityContactText = (currentValidator.description.security_contact)
+      ? currentValidator.description.security_contact
+      : "—";
 
-    if (currentValidator.description.details) {
-      detailsText = currentValidator.description.details;
-    } else { 
-      detailsText = "—";
-    }
-
-  } else {
+    detailsText = (currentValidator.description.details)
+      ? currentValidator.description.details
+      : "—";
+  } 
+  
+  // РЕНДЕР ДЛЯ НЕ СУЩЕСТВУЮЩЕГО ВАЛИДАТОРА
+  else {
     avatarUrl = `${process.env["PUBLIC_URL"]}/images/no-avatar.png`;
-    monikerText = "Validator Doesn't Exist";
-    valoperText = "no validator operator address";
+    monikerText = "Oops! Validator Doesn't Exist";
+    valoperText = "Here could be a validator operator address";
     activityText = "No activity";
     activityStyle = "validator__status validator__status_neutral";
     bondText = "";
@@ -147,122 +147,120 @@ function Validator() {
   }
 
 
+  // if (currentValidator) {
 
+  //   if (currentValidator.avatar) {
+  //     avatarUrl = currentValidator.avatar
+  //   } else {
+  //     avatarUrl = `${process.env["PUBLIC_URL"]}/images/no-avatar.png`
+  //   };
 
+  //   monikerText = currentValidator.description.moniker;
+  //   valoperText = currentValidator.operator_address;
 
+  //   if (currentValidator.status === "BOND_STATUS_BONDED") {
+  //     activityText = "Active";
+  //     activityStyle = "validator__status validator__status_good";
+  //   } else {
+  //     activityText = "Inactive";
+  //     activityStyle = "validator__status validator__status_neutral";
+  //   }
 
+  //   if (currentValidator.status === 'BOND_STATUS_BONDED') {
+  //     bondText = "Bonded";
+  //     bondStyle = "validator__status validator__status_good";
+  //   } else if (currentValidator.status === 'BOND_STATUS_UNBONDING') {
+  //     bondText = "Unbonding";
+  //     bondStyle = "validator__status validator__status_special";
+  //   } else {
+  //     bondText = "Unbonded";
+  //     bondStyle = "validator__status validator__status_neutral";
+  //   }
 
+  //   if (currentValidator.jailed) {
+  //     jailText = "Jailed";
+  //     jailStyle = "validator__status validator__status_bad";
+  //   } else {
+  //     jailText = "";
+  //     jailStyle = "validator__status validator__status_hidden";
+  //   }
 
+  //   if (Number(currentValidator.commission.commission_rates.rate) > 0.1) {
+  //     highCommissionText = "High %";
+  //     highCommissionStyle = "validator__status validator__status_bad";
+  //   } else {
+  //     highCommissionText = "";
+  //     highCommissionStyle = "validator__status validator__status_hidden";
+  //   }
 
+  //   if (currentValidator.rank) {
+  //     rankText = "#" + currentValidator.rank.toString().padStart(3, '0');
+  //   } else {
+  //     rankText = "#000";
+  //   }
 
+  //   if (currentChain) {
+  //     tokensText = tweakTokens(currentValidator.tokens, currentChain);
+  //     symbolText = currentChain.symbol;
+  //   } else {
+  //     tokensText = "—";
+  //     symbolText = "";
+  //   }
 
-  // ПРИ ОТКРЫТИИ КОМПОНЕНТА СКРЫВАЕМ ТАБЛИЦУ ВАЛИДАТОРОВ
-  useEffect(() => {
-    setIsValidatorsHidden(true);
-  }, [])
+  //   if (currentValidator.voting_power && currentChain) {
+  //     votingPowerText = tweakVotingPower(currentValidator.voting_power, currentChain) + '%'
+  //   } else {
+  //     votingPowerText = "";
+  //   }
 
-  // ВОЗВРАТ К ТАБЛИЦЕ ВАЛИДАТОРОВ
-  const returnToValidators = () => {
-    navigate(`/${currentChain?.chainId}/validators`);
-    setIsValidatorsHidden(false);
-  }
+  //   commissionText = tweakCommission(currentValidator.commission.commission_rates.rate) + '%';
+  //   if (Number(currentValidator.commission.commission_rates.rate) > 0.1) {
+  //     commissionStyle = "validator__commission-value validator__commission-value_high";
+  //   } else {
+  //     commissionStyle = "validator__commission-value";
+  //   }
 
-  // ПОЛУЧАЕМ ОБЪЕКТ ТЕКУЩЕГО ВАЛИДАТОРА
-  useEffect(() => {
-    const validator = validators?.find((val: { operator_address: string }) => val.operator_address === currentValoper);
-    if (validator) setCurrentValidator(validator);
-    else setCurrentValidator(null);
-  }, [currentValoper, currentChain, validators])
+  //   /* В ранней версии вместо websiteText у меня был websiteEl, который в случае отсутствия ссылки превращался в текстовый прочерк, а в случае наличия - в кликабельную ссылку. Однако я обнаружил, что если валидатор указал ссылку не в формате http://site.com, а просто как site.com, то её нельзя передавать в href - ссылка не будет корректно работать. Таких валидаторов меньшинство, но всё же. По этой причине решил пока оставить сайт обычным текстом, но позже возможно напишу для ссылок валидацию, чтобы все варианты работали корректно.  */
+  //   if (currentValidator.description.website) {
+  //     websiteText = currentValidator.description.website;
+  //   } else { 
+  //     websiteText = "—";
+  //   }
 
-  // // АВАТАР
-  // const avatarUrl = (currentValidator?.avatar)
-  //   ? currentValidator.avatar
-  //   : `${process.env["PUBLIC_URL"]}/images/no-avatar.png`;
+  //   if (currentValidator.description.security_contact) {
+  //     securityContactText = currentValidator.description.website;
+  //   } else { 
+  //     securityContactText = "—";
+  //   }
 
-  // // РЕЙТИНГ
-  // const rank = '#' + currentValidator?.rank?.toString().padStart(3, '0');
+  //   if (currentValidator.description.details) {
+  //     detailsText = currentValidator.description.details;
+  //   } else { 
+  //     detailsText = "—";
+  //   }
 
-  // // МОНИКЕР
-  // const moniker = currentValidator?.description.moniker;
-
-  // // ВАЛОПЕР
-  // const valoper = currentValidator?.operator_address;
-
-  // // АКТИВНОСТЬ
-  // const activityText = (currentValidator?.status === 'BOND_STATUS_BONDED')
-  //   ? 'Active'
-  //   : 'Inactive';
-
-  // const activityStyle = (currentValidator?.status === 'BOND_STATUS_BONDED')
-  //   ? 'validator__status validator__status_good'
-  //   : 'validator__status validator__status_neutral';
-
-  // // БОНД
-  // const bondText =
-  //   (currentValidator?.status === 'BOND_STATUS_BONDED')
-  //     ? 'Bonded'
-  //     : (currentValidator?.status === 'BOND_STATUS_UNBONDED')
-  //       ? 'Unbonded'
-  //       : (currentValidator?.status === 'BOND_STATUS_UNBONDING')
-  //         ? 'Unbonding'
-  //         : '';
-
-  // const bondStyle =
-  //   (currentValidator?.status === 'BOND_STATUS_BONDED')
-  //     ? 'validator__status validator__status_good'
-  //     : (currentValidator?.status === 'BOND_STATUS_UNBONDED')
-  //       ? 'validator__status validator__status_neutral'
-  //       : (currentValidator?.status === 'BOND_STATUS_UNBONDING')
-  //         ? 'validator__status validator__status_special'
-  //         : '';
-
-  // // ТЮРЬМА
-  // const jailText = (currentValidator?.jailed) ? 'Jailed' : '';
-  // const jailStyle = (currentValidator?.jailed)
-  //   ? 'validator__status validator__status_bad'
-  //   : 'validator__status validator__status_hidden';
-
-  // // ВЫСОКАЯ КОМИССИЯ
-  // const highCommissionText = (Number(currentValidator?.commission.commission_rates.rate) > 0.1)
-  //   ? 'High %'
-  //   : '';
-
-  // const highCommissionStyle = (Number(currentValidator?.commission.commission_rates.rate) > 0.1)
-  //   ? 'validator__status validator__status_bad'
-  //   : 'validator__status validator__status_hidden';
-
-
-  // // РЕНДЕР ВЕСА ГОЛОСА
-  // const stake = (currentChain && currentValidator)
-  //   ? tweakTokens(currentValidator.tokens, currentChain)
-  //   : '';
-  // const symbol = currentChain?.symbol;
-  // const votingPower = (currentChain && currentValidator?.voting_power)
-  //   ? tweakVotingPower(currentValidator.voting_power, currentChain) + '%'
-  //   : '';
-
-  // // РЕНДЕР КОМИССИИ
-  // const commissionText = (currentValidator)
-  //   ? tweakCommission(currentValidator.commission.commission_rates.rate) + '%'
-  //   : '';
-
-  // const commissionStyle = (currentValidator && Number(currentValidator.commission.commission_rates.rate) > 0.1)
-  //   ? 'validator__commission-value validator__commission-value_high'
-  //   : 'validator__commission-value';
-
-  // // РЕНДЕР ВЕБСАЙТА
-  // const website = (currentValidator?.description.website === '')
-  //   ? <p className="validator__data-text">—</p>
-  //   : <a className="validator__data-link" href={currentValidator?.description.website} target="_blank">{currentValidator?.description.website}</a>
-
-  // // РЕНДЕР БЕЗОПАСНОГО КОНТАКТА
-  // const securityContact = (currentValidator?.description.security_contact === '') ? '—' : currentValidator?.description.security_contact;
-
-  // // РЕНДЕР ОПИСАНИЯ
-  // const details = (currentValidator?.description.details === '') ? '—' : currentValidator?.description.details;
-
-
-
+  // } else {
+  //   avatarUrl = `${process.env["PUBLIC_URL"]}/images/no-avatar.png`;
+  //   monikerText = "Oops! Validator Doesn't Exist";
+  //   valoperText = "Here could be a validator operator address";
+  //   activityText = "No activity";
+  //   activityStyle = "validator__status validator__status_neutral";
+  //   bondText = "";
+  //   bondStyle = "validator__status validator__status_hidden";
+  //   jailText = "";
+  //   jailStyle = "validator__status validator__status_hidden";
+  //   highCommissionText = "";
+  //   highCommissionStyle = "validator__status validator__status_hidden";
+  //   rankText = "#000";
+  //   tokensText = "—";
+  //   symbolText = "";
+  //   votingPowerText = "—";
+  //   commissionText = "—";
+  //   commissionStyle = "validator__commission-value";
+  //   websiteText = "—";
+  //   securityContactText = "—";
+  //   detailsText = "—";
+  // }
 
   return (
     <div className="validator">
--- src/components/ValidatorsTableRow.tsx
+++ src/components/ValidatorsTableRow.tsx
@@ -19,74 +19,64 @@ function ValidatorsTableRow(props: IValidatorsTableRowProps) {
   const currentChain = useAppSelector(selectCurrentChain);
   const validator = props.validator;
 
-  // АВАТАР
-  const avatarUrl = (validator.avatar)
+  let avatarUrl, monikerText, activityText, activityStyle, bondText, bondStyle, jailText, jailStyle, highCommissionText, highCommissionStyle, rankText, tokensText, symbolText, votingPowerText, commissionText, commissionStyle;
+
+  avatarUrl = (validator.avatar)
     ? validator.avatar
     : `${process.env["PUBLIC_URL"]}/images/no-avatar.png`;
 
-  // РЕЙТИНГ
-  const rank = '#' + validator.rank?.toString().padStart(3, '0');
+  monikerText = validator.description.moniker;
 
-  // МОНИКЕР
-  const moniker = validator.description.moniker;
+  if (validator.status === 'BOND_STATUS_BONDED') {
+    activityText = "Active";
+    activityStyle = "validators-tr__status validators-tr__status_good";
+  } else {
+    activityText = "Inactive";
+    activityStyle = "validators-tr__status validators-tr__status_neutral";
+  }
 
-  // АКТИВНОСТЬ
-  const activityText = (validator.status === 'BOND_STATUS_BONDED')
-    ? 'Active'
-    : 'Inactive';
+  if (validator.status === 'BOND_STATUS_BONDED') {
+    bondText = "Bonded";
+    bondStyle = "validators-tr__status validators-tr__status_good";
+  } else if (validator.status === 'BOND_STATUS_UNBONDING') {
+    bondText = "Unbonding";
+    bondStyle = "validators-tr__status validators-tr__status_special";
+  } else {
+    bondText = "Unbonded";
+    bondStyle = "validators-tr__status validators-tr__status_neutral";
+  }
 
-  const activityStyle = (validator.status === 'BOND_STATUS_BONDED')
-    ? 'validators-tr__status validators-tr__status_good'
-    : 'validators-tr__status validators-tr__status_neutral';
+  if (validator.jailed) {
+    jailText = "Jailed";
+    jailStyle = "validators-tr__status validators-tr__status_bad";
+  } else {
+    jailText = "";
+    jailStyle = "validators-tr__status validators-tr__status_hidden";
+  }
 
-  // БОНД
-  const bondText =
-    (validator.status === 'BOND_STATUS_BONDED')
-      ? 'Bonded'
-      : (validator.status === 'BOND_STATUS_UNBONDED')
-        ? 'Unbonded'
-        : (validator.status === 'BOND_STATUS_UNBONDING')
-          ? 'Unbonding'
-          : '';
+  if ((Number(validator.commission.commission_rates.rate) > 0.1)) {
+    highCommissionText = "High %";
+    highCommissionStyle = "validators-tr__status validators-tr__status_bad";
+    commissionStyle = "validators-tr__commission-value validators-tr__commission-value_high";
+  } else {
+    highCommissionText = "";
+    highCommissionStyle = "validators-tr__status validators-tr__status_hidden";
+    commissionStyle = "validators-tr__commission-value";
+  }
 
-  const bondStyle =
-    (validator.status === 'BOND_STATUS_BONDED')
-      ? 'validators-tr__status validators-tr__status_good'
-      : (validator.status === 'BOND_STATUS_UNBONDED')
-        ? 'validators-tr__status validators-tr__status_neutral'
-        : (validator.status === 'BOND_STATUS_UNBONDING')
-          ? 'validators-tr__status validators-tr__status_special'
-          : '';
+  if (currentChain) {
+    tokensText = tweakTokens(validator.tokens, currentChain);
+    symbolText = currentChain.symbol;
+  } else {
+    tokensText = "—";
+    symbolText = "";
+  }
 
-  // ТЮРЬМА
-  const jailText = (validator.jailed) ? 'Jailed' : '';
-  const jailStyle = (validator.jailed)
-    ? 'validators-tr__status validators-tr__status_bad'
-    : 'validators-tr__status validators-tr__status_hidden';
+  votingPowerText = (validator.voting_power && currentChain)
+    ? tweakVotingPower(validator.voting_power, currentChain) + "%"
+    : "";
 
-  // ВЫСОКАЯ КОМИССИЯ
-  const highCommissionText = (Number(validator.commission.commission_rates.rate) > 0.1)
-    ? 'High %'
-    : '';
-
-  const highCommissionStyle = (Number(validator.commission.commission_rates.rate) > 0.1)
-    ? 'validators-tr__status validators-tr__status_bad'
-    : 'validators-tr__status validators-tr__status_hidden';
-
-  // РЕНДЕР ВЕСА ГОЛОСА
-  const stake = (currentChain)
-    ? tweakTokens(validator.tokens, currentChain)
-    : '';
-  const symbol = currentChain?.symbol;
-  const votingPower = (currentChain && validator.voting_power)
-    ? tweakVotingPower(validator.voting_power, currentChain) + '%'
-    : '';
-
-  // РЕНДЕР КОМИССИИ
-  const commissionText = tweakCommission(validator.commission.commission_rates.rate) + '%';
-  const commissionStyle = (Number(validator.commission.commission_rates.rate) > 0.1)
-    ? 'validators-tr__commission-value validators-tr__commission-value_high'
-    : 'validators-tr__commission-value';
+  commissionText = tweakCommission(validator.commission.commission_rates.rate) + "%";
 
   return (
     <Link to={`/${currentChain?.chainId}/validators/${validator.operator_address}`} className="validators-tr">
@@ -96,8 +86,8 @@ function ValidatorsTableRow(props: IValidatorsTableRowProps) {
         <div style={{ backgroundImage: `url("${avatarUrl}")` }} className="validators-tr__avatar"></div>
         <div className="validators-tr__info">
           <div className="validators-tr__alignment">
-            <span className="validators-tr__rank">{rank}</span>
-            <span className="validators-tr__moniker">{moniker}</span>
+            <span className="validators-tr__rank">{rankText}</span>
+            <span className="validators-tr__moniker">{monikerText}</span>
           </div>
           <div className="validators-tr__statuses">
             <span className={activityStyle}>{activityText}</span>
@@ -110,10 +100,10 @@ function ValidatorsTableRow(props: IValidatorsTableRowProps) {
 
       {/* ВЕС ГОЛОСА */}
       <div className="validators-tr__voting-power">
-        <span className="validators-tr__stake">{stake}
-          <span className="validators-tr__symbol">{symbol}</span>
+        <span className="validators-tr__stake">{tokensText}
+          <span className="validators-tr__symbol">{symbolText}</span>
         </span>
-        <span className="validators-tr__power">{votingPower}</span>
+        <span className="validators-tr__power">{votingPowerText}</span>
       </div>
 
       {/* КОМИССИЯ */}

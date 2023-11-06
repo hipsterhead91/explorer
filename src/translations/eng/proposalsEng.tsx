a8cb20e8469ca05fbba5164c70035798c4d0d3bb
hipsterhead91 <virtualxself@gmail.com>
2023-11-06 21:58:41 +0200

    end dark theme

--- src/translations/eng/proposalsEng.tsx
+++ src/translations/eng/proposalsEng.tsx
@@ -1,6 +1,6 @@
 const proposalsEng = {
   disclaimerSpan: "This section is work in progress.",
-  disclaimer: " Some elements may not be displayed correctly.",
+  disclaimer: " Some elements, such as proposal votes, its title or description, may not be displayed correctly.",
   proposal: "Proposal",
   status: "Status",
   type: "Type",
